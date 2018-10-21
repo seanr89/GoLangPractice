c16b85738e4068a9e21ac5e906fd067f2672f14f
Sean <srafferty89@gmail.com>
2018-10-21 20:08:39 +0100

    testing creating a new file and executing imports

--- MyFirstApp/hello.go
+++ MyFirstApp/hello.go
@@ -6,11 +6,21 @@
 package main
 
 import "fmt"
+import "./stringUtil"
 
 func main() {
 
 	//initialise a variable to store the output
 	var message string
+	var a, b, c int
 	message = "Hello go world!"
-	fmt.Printf(message)
+	fmt.Println(message, a, b, c)
+
+	//now lets create a for loop
+	for i:=0; i < 5; i++{
+		fmt.Println("i is currently : ", i)
+	}
+
+	//utilizing package import and method calling
+	fmt.Println(stringUtil.Reverse("!oG ,olleH"))
 }
