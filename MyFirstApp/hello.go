9ef223fc94db69012447ec280900f517d0c29d2c
Sean <srafferty89@gmail.com>
2018-10-21 11:29:01 +0100

    gitignore and basic type added :)

--- MyFirstApp/hello.go
+++ MyFirstApp/hello.go
@@ -1,7 +1,16 @@
+/**
+* Created using VSCode
+* User : Sean
+* Basic main class to test hello world tools
+*/
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Printf("hello, world\n")
+
+	//initialise a variable to store the output
+	var message string
+	message = "Hello go world!"
+	fmt.Printf(message)
 }
