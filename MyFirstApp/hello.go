a61c31b6120ffde2b16d117acd723f550cfca963
Sean <srafferty89@gmail.com>
2018-10-21 21:46:19 +0100

    testing deferred types in go

--- MyFirstApp/hello.go
+++ MyFirstApp/hello.go
@@ -23,4 +23,30 @@ func main() {
 
 	//utilizing package import and method calling
 	fmt.Println(stringUtil.Reverse("!oG ,olleH"))
+
+	//testing the executing of method calls from the file itself as well
+	doDBOperations()
+}
+
+func connectToDB () {
+    fmt.Println( "ok, connected to db" )
+}
+
+func disconnectFromDB () {
+    fmt.Println( "ok, disconnected from db" )
+}
+
+/*
+testing with deferred operations calls (to be executed just before returning from the current function block)
+*/
+func doDBOperations() {
+	connectToDB()
+	fmt.Println("Defering the database disconnect.")
+	defer disconnectFromDB() //function called here with defer
+	fmt.Println("Doing some DB operations ...")
+	fmt.Println("Oops! some crash or network error ...")
+	fmt.Println("Returning from function here!")
+	return //terminate the program
+
+	// deferred function executed here just before actually returning, even if there is a return or abnormal termination before
 }
