9f30430962580b0161704e7a331867949e1028a6
seanr89 <srafferty89@gmail.com>
2018-11-10 19:50:56 +0000

    code added for creation and deletion

--- MyAPI/main.go
+++ MyAPI/main.go
@@ -23,6 +23,7 @@ type Person struct {
 	Lastname  string   `json:"lastname,omitempty"`
 	Address   *Address `json:"address,omitempty"`
 }
+
 type Address struct {
 	City  string `json:"city,omitempty"`
 	State string `json:"state,omitempty"`
@@ -56,16 +57,26 @@ func GetPerson(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(&Person{})
 }
 
-/**
- */
+// create a new item
 func CreatePerson(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	params := mux.Vars(r)
+	var person Person
+	_ = json.NewDecoder(r.Body).Decode(&person)
+	person.ID = params["id"]
+	people = append(people, person)
+	json.NewEncoder(w).Encode(people)
 }
 
-/**
- */
+// Delete an item
 func DeletePerson(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "not implemented yet !")
+	params := mux.Vars(r)
+	for index, item := range people {
+		if item.ID == params["id"] {
+			people = append(people[:index], people[index+1:]...)
+			break
+		}
+		json.NewEncoder(w).Encode(people)
+	}
 }
 
 ///////////////////////////////////////////////////////////////
