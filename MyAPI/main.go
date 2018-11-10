82916da52c3dab4ab212b3c239c18fc6d3a79020
seanr89 <srafferty89@gmail.com>
2018-11-10 19:39:12 +0000

    structure objects added with some example base calls

--- MyAPI/main.go
+++ MyAPI/main.go
@@ -6,6 +6,7 @@ go get github.com/gorilla/mux
 */
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -13,29 +14,53 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type Person struct {
+	ID        string   `json:"id,omitempty"`
+	Firstname string   `json:"firstname,omitempty"`
+	Lastname  string   `json:"lastname,omitempty"`
+	Address   *Address `json:"address,omitempty"`
+}
+type Address struct {
+	City  string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
+var people []Person
+
 //Our main function
 func main() {
 	fmt.Println("My First API")
 
 	router := mux.NewRouter()
+	//Append dummy data to people array for api output
+	people = append(people, Person{ID: "1", Firstname: "John", Lastname: "Doe", Address: &Address{City: "City X", State: "State X"}})
+	people = append(people, Person{ID: "2", Firstname: "Sean", Lastname: "Rafferty", Address: &Address{City: "City Z", State: "State Y"}})
+	people = append(people, Person{ID: "3", Firstname: "Francis", Lastname: "Sunday"})
+
 	//implement endpoints for API
 	router.HandleFunc("/people", GetPeople).Methods("GET")
 	router.HandleFunc("/people/{id}", GetPerson).Methods("GET")
 	router.HandleFunc("/people/{id}", CreatePerson).Methods("POST")
 	router.HandleFunc("/people/{id}", DeletePerson).Methods("DELETE")
-	log.Fatal(http.ListenAndServe(":8000", router))
+	if err := http.ListenAndServe(":8000", router); err != nil {
+		log.Fatal(err)
+	}
 }
 
 /**
- */
+Request and return all people(JSON)
+*/
 func GetPeople(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "not implemented yet !")
+	json.NewEncoder(w).Encode(people)
 }
 
 /**
  */
 func GetPerson(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "not implemented yet !")
+	//access params passed to API
+	//params := mux.Vars(r)
 }
 
 /**
